e9b5dce2b8226a0c8e8da475f0e5ace605fee8dc
titaaknt <agnestitania3@gmail.com>
2024-10-01 13:53:34 +0700

    Program Pemilihan2Percobaan101

---
+++ Pemilihan2Percobaan101.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan101 {
+    public static void main(String[] args) {
+        Scanner input01 = new Scanner(System.in);
+
+        System.out.print("masukkan tahun: ");
+        int tahun = input01.nextInt();
+        
+        if ((tahun % 4) == 0) {
+            if ((tahun %100) != 0)
+                System.out.println("Tahun Kabisat");
+        } else
+            System.out.println("Bukan Tahun Kabisat");
+    }
+}
