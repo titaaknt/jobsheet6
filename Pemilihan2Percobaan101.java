15c4ede0ce132945472edcf3ac0d3e3371930089
titaaknt <agnestitania3@gmail.com>
2024-10-01 15:48:50 +0700

    Add files via upload

--- Pemilihan2Percobaan101.java
+++ Pemilihan2Percobaan101.java
@@ -9,6 +9,8 @@ public class Pemilihan2Percobaan101 {
         if ((tahun % 4) == 0) {
             if ((tahun %100) != 0)
                 System.out.println("Tahun Kabisat");
+            else
+                System.out.println("Bukan Tahun Kabisat");
         } else
             System.out.println("Bukan Tahun Kabisat");
     }
